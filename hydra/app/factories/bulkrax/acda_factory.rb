0df48c45f5ce743c5c7909461727e2fd79b74f8f
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-02-16 10:09:11 -0800

    i35 AcdaFactory #create method (#42)
    * copy attr transformation logic from Bulkrax, remove unnecessary or Hyrax-specific pieces
    
    * filter out filepath fields when transforming attrs
    
    * finalize #create method
    
    * add TODO re: file path fields

--- hydra/app/factories/bulkrax/acda_factory.rb
+++ hydra/app/factories/bulkrax/acda_factory.rb
@@ -4,6 +4,27 @@ module Bulkrax
   # Used to create Acda records when performing Bulkrax imports.
   # Based on Bulkrax's own Bulkrax::ObjectFactory.
   class AcdaFactory
+    # @api private
+    #
+    # These are the attributes that we assume all "work type" classes (e.g. the given :klass) will
+    # have in addition to their specific attributes.
+    #
+    # @return [Array<Symbol>]
+    # @see #permitted_attributes
+    class_attribute :base_permitted_attributes,
+                    default: %i[id edit_users edit_groups read_groups visibility]
+
+    # @return [Boolean]
+    #
+    # @example
+    #   Bulkrax::ObjectFactory.transformation_removes_blank_hash_values = true
+    #
+    # @see #transform_attributes
+    # @see https://github.com/samvera-labs/bulkrax/pull/708 For discussion concerning this feature
+    # @see https://github.com/samvera-labs/bulkrax/wiki/Interacting-with-Metadata For documentation
+    #      concerning default behavior.
+    class_attribute :transformation_removes_blank_hash_values, default: false
+
     attr_reader :attributes, :object, :source_identifier_value,
                 :klass, :replace_files, :update_files, :work_identifier,
                 :related_parents_parsed_mapping, :importer_run_id
@@ -29,8 +50,12 @@ module Bulkrax
       object
     end
 
-    # TODO: implement
-    def create; end
+    def create
+      @object = klass.new(transform_attributes)
+
+      object.save!
+      log_created(object)
+    end
 
     # TODO: implement
     def update; end
@@ -51,6 +76,68 @@ module Bulkrax
       # in the multivalued field, so we have to go through them one at a time.
       match = klass.where(query).detect { |m| m.send(work_identifier).include?(source_identifier_value) }
       return match if match
+    end
+
+    # Override if we need to map the attributes from the parser in
+    # a way that is compatible with how the factory needs them.
+    def transform_attributes(update: false)
+      @transform_attributes = attributes.slice(*permitted_attributes)
+      # TODO: implement with files or remove
+      # @transform_attributes.merge!(file_attributes(update_files)) if with_files
+      @transform_attributes = remove_blank_hash_values(@transform_attributes) if transformation_removes_blank_hash_values?
+      # TODO: Should we filter these out earlier in the process? I.e. before they get into :attributes or when we're
+      #       parsing the metadata? I don't think we currently know enough about what :attributes will look like
+      #       at this point to say for sure yet.
+      exceptions = update ? [:id] + file_path_field_names : file_path_field_names
+
+      @transform_attributes.except(exceptions)
+    end
+
+    # Regardless of what the Parser gives us, these are the properties we are prepared to accept.
+    def permitted_attributes
+      klass.properties.keys.map(&:to_sym) + base_permitted_attributes
+    end
+
+    # When creating an Acda record, these all get filtered out of the metadata.
+    # @see ImportLibrary#create_new_record
+    #
+    # @return [Array<Symbol>] List of all ingestable <file>_path field names
+    def file_path_field_names
+      %i[
+        audio_path
+        image_path
+        pdf_path
+        thumb_path
+        video_path
+        pdf_image_path
+        pdf_thumb_path
+        video_image_path
+        video_thumb_path
+      ]
+    end
+
+    # Return a copy of the given attributes, such that all values that are empty or an array of all
+    # empty values are fully emptied.  (See implementation details)
+    #
+    # @param attributes [Hash]
+    # @return [Hash]
+    #
+    # @see https://github.com/emory-libraries/dlp-curate/issues/1973
+    def remove_blank_hash_values(attributes)
+      dupe = attributes.dup
+      dupe.each do |key, values|
+        if values.is_a?(Array) && values.all? { |value| value.is_a?(String) && value.empty? }
+          dupe[key] = []
+        elsif values.is_a?(String) && values.empty?
+          dupe[key] = nil
+        end
+      end
+      dupe
+    end
+
+    def log_created(obj)
+      msg = "Created #{obj.class} #{obj.id}"
+      Rails.logger.info("#{msg} (#{Array(attributes[work_identifier]).first})")
     end
 
     # This method mimics the User.batch_user method in a Hyrax app.
