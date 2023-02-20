0cee39b1f5c7e5220d8084e22b53d3b2b83c6b59
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-02-20 18:05:40 -0500

    updated schedule.rb

--- hydra/config/schedule.rb
+++ hydra/config/schedule.rb
@@ -8,16 +8,17 @@ set :environment, ENV['RAILS_ENV']
 # if rails env is production run cron_import every hour
 # if ENV['RAILS_ENV'] == 'production'
 #   every 1.hour do
-#     command "cd #{path} && bin/rails r import/cron_import.rb acda_portal"
+#     command "cd #{path} && bin/rails r import/cron_import.rb acda_portal_public"
 #   end
 # else
 #   every 5.minute do
-#     command "cd #{path} && bin/rails r import/cron_import.rb acda_portal"
+#     command "cd #{path} && bin/rails r import/cron_import.rb acda_portal_public"
 #   end
 # end
 
 # clobber the tmp folder daily and logs to keep files small 
 every 1.day do
-  command "cd #{path} && rake log:clear"
+  command "cd #{path} && bundle exec rake log:clear"
   command "cd #{path} && bin/rails tmp:clear"
+  command "cd #{path} && bin/rails restart" # restart the server
 end
