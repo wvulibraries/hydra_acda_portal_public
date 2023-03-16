6f603953595ca2af590ca1fff1fda73551293b76
Summer Cook <summer@scientist.com>
2023-03-16 15:35:25 -0500

    add auth for bulkrax routes

--- hydra/app/controllers/application_controller.rb
+++ hydra/app/controllers/application_controller.rb
@@ -2,8 +2,20 @@
 class ApplicationController < ActionController::Base
   include Blacklight::Controller
   include Hydra::Controller::ControllerBehavior
+  before_action :authenticate_if_needed
 
   layout 'collection'
 
   protect_from_forgery with: :exception
+
+  # Extra authentication for bulkrax
+  def authenticate_if_needed
+    # Disable this extra authentication in test mode
+    # return true if Rails.env.test?
+    if (controller_path.include?('bulkrax'))
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "wvu" && password == "hydra"
+      end
+    end
+  end
 end
