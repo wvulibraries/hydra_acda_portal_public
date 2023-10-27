c6b657f1bfb81d9c9bf3c5cb2c4c30fb1d0cbd3e
Shana Moore <shana@scientist.com>
2023-10-27 13:33:21 -0700

    🧹 Make corrections for CSL per client QA
    In this commit we are making adjustments to the Chicago Citation Service to make the requirements specified by the client.
    
    ref:
    - https://github.com/scientist-softserv/west-virginia-university/issues/113#issuecomment-1783385472
    
    Co-Authored-By: Kirk Wang <k3wang@gmail.com>

--- hydra/app/services/chicago_citation_service.rb
+++ hydra/app/services/chicago_citation_service.rb
@@ -4,18 +4,16 @@ module ChicagoCitationService
       # format is based off of https://www.loc.gov/programs/teachers/getting-started-with-primary-sources/citing/chicago/
       text = ""
 
-      creator = sanitize_value(document['creator_tesim']&.first)
-      title = sanitize_value(document['title_tesim']&.first || document['description_tesim']&.first)
-      date = sanitize_value(document['date_tesim']&.first)
-      location = sanitize_value(physical_location(document))
-      contributing_institution = sanitize_value(document['contributing_institution_tesim']&.first)
+      title = sanitize_value(document['title_ssi'] || document['description_ssi'])
+      date = sanitize_value(document['date_ssi'])
+      location = sanitize_value(document['physical_location_ssi'])
+      contributing_institution = sanitize_value(document['contributing_institution_ssi'])
       url = original_url
       access_date = Date.today.strftime("%B %d, %Y")
 
-      text << "#{creator}. " if creator.present?
-      text << "<i>#{title}</i>. " if title.present?
-      text << "#{date}. " if date.present?
-      text << "#{location}. " if location.present?
+      text << "<i>#{title}</i>, " if title.present?
+      text << "#{date}, " if date.present?
+      text << "#{location}, " if location.present?
       text << "#{contributing_institution}. " if contributing_institution.present?
       text << "#{url}"
       text << " (accessed #{access_date})."
@@ -26,35 +24,18 @@ module ChicagoCitationService
     private
 
       def collection_number(doc)
-        match = doc['physical_location_tesim']&.first&.match(/collection (\d+)/i)
+        match = doc['physical_location_ssi']&.match(/collection (\d+)/i)
         match ? match[1] : nil
       end
 
       def box_number(doc)
-        match = doc['physical_location_tesim']&.first&.match(/box (\d+)/i)
+        match = doc['physical_location_ssi']&.match(/box (\d+)/i)
         match ? match[1] : nil
       end
 
       def folder_number(doc)
-        match = doc['physical_location_tesim']&.first&.match(/folder (\d+)/i)
+        match = doc['physical_location_ssi']&.match(/folder (\d+)/i)
         match ? match[1] : nil
-      end
-
-      def physical_location(doc)
-        collection = collection_number(doc)
-        box = box_number(doc)
-        folder = folder_number(doc)
-
-        if collection || box || folder
-          location = ""
-          location << "Collection #{collection}, " if collection
-          location << "Box #{box}, " if box
-          location << "Folder #{folder}" if folder
-
-          location.strip.chomp(',')
-        else
-          doc['physical_location_tesim']&.first
-        end
       end
 
       def sanitize_value(value)
