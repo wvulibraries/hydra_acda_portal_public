55afad9b3cdb5a7b188d1008b8c7f577b1801ed6
Kirk Wang <kirk.wang@scientist.com>
2023-10-31 09:30:54 -0700

    🐛 Export identifier without congress archives URL
    This commit will strip the congress archives URL from the identifier
    when exporting catalog results to CSV or XML format.
    
    Also, fixed specs that were failing due to some changes.
    
    Ref:
      - https://github.com/scientist-softserv/west-virginia-university/issues/93
      - https://github.com/scientist-softserv/west-virginia-university/issues/94

--- hydra/spec/presenters/export_results_presenter_spec.rb
+++ hydra/spec/presenters/export_results_presenter_spec.rb
@@ -124,7 +124,7 @@ RSpec.describe ExportResultsPresenter do
     subject { described_class.new(raw_response).to_csv }
 
     it { is_expected.to be_a(String) }
-    it { is_expected.to eq("dcterms:provenance,dcterms:title,dcterms:date,dcterms:created,dcterms:creator,dcterms:rights,dcterms:language,dcterms:temporal,dcterms:relation,dcterms:isPartOf,dcterms:source,dcterms:identifier,edm:preview,edm:isShownAt,dcterms:http://purl.org/dc/terms/type,dcterms:subject,dcterms:contributor,dcterms:description,dcterms:type\nRobert J. Dole Institute of Politics,Dole and Goldwater shake hands,1964,1964,Unknown,http://rightsstatements.org/vocab/NKC/1.0/,zxx,88th (1963-1964),\"Dole Photograph Collection, 1900-2011\",\"Collection 012, Box 6, Folder 15\",https://dolearchivecollections.ku.edu/index.php?p=collections/controlcard&id=47&q=,http://congressarchivesdev.lib.wvu.edu/record/ph_006_015_002,https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323,https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323,black-and-white photograph,Government Operations and Politics,\"Goldwater, Barry M. (Barry Morris), 1909-1998; Dole, Robert J., 1923-2021\",Congressman Bob Dole and Senator Barry Goldwater shaking hands with an American flag in the background.,StillImage\nRobert J. Dole Institute of Politics,\"Kassebaum-Baker, Sen. Nancy, 4/16/2009\",2009-04-16,2009-04-16,\"Williams, Brien R.\",http://rightsstatements.org/vocab/CNE/1.0/,eng,111th (2009-2010),\"Dole Institute Oral History Project, 2002-2009\",\"Collection 018, Nancy Kassebaum-Baker Oral History from 2009-04-16\",https://dolearchivecollections.ku.edu/index.php?p=collections/findingaid&id=51&q=,http://congressarchivesdev.lib.wvu.edu/record/LSqyFpjhe3g,https://www.youtube.com/watch?v=LSqyFpjhe3g,https://dolearchives.omeka.net/items/show/234,moving image,\"Agriculture and Food, Health, International Affairs\",\"Dole, Robert J., 1923-2021; Baker, Nancy Kassebaum, 1932-\",\"In this 2009 oral history interview with Brien R. Williams, Kassebaum-Baker talks about Senator Dole's work ethic and the issues of abortion, the Panama Canal, and wheat subsidies on the campaign trail.  Baker represented Kansas in the U.S. Senate (1978-1997). She was the first female senator not elected to a seat held by her husband nor appointed to fill out a deceased husband’s term.\",MovingImage\n") }
+    it { is_expected.to eq("dcterms:provenance,dcterms:title,dcterms:date,dcterms:created,dcterms:creator,dcterms:rights,dcterms:language,dcterms:temporal,dcterms:relation,dcterms:isPartOf,dcterms:source,dcterms:identifier,edm:preview,edm:isShownAt,dcterms:http://purl.org/dc/terms/type,dcterms:subject,dcterms:contributor,dcterms:description,dcterms:type\nRobert J. Dole Institute of Politics,Dole and Goldwater shake hands,1964,1964,Unknown,http://rightsstatements.org/vocab/NKC/1.0/,zxx,88th (1963-1964),\"Dole Photograph Collection, 1900-2011\",\"Collection 012, Box 6, Folder 15\",https://dolearchivecollections.ku.edu/index.php?p=collections/controlcard&id=47&q=,ph_006_015_002,https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323,https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323,black-and-white photograph,Government Operations and Politics,\"Goldwater, Barry M. (Barry Morris), 1909-1998; Dole, Robert J., 1923-2021\",Congressman Bob Dole and Senator Barry Goldwater shaking hands with an American flag in the background.,StillImage\nRobert J. Dole Institute of Politics,\"Kassebaum-Baker, Sen. Nancy, 4/16/2009\",2009 April 16,2009-04-16,\"Williams, Brien R.\",http://rightsstatements.org/vocab/CNE/1.0/,eng,111th (2009-2010),\"Dole Institute Oral History Project, 2002-2009\",\"Collection 018, Nancy Kassebaum-Baker Oral History from 2009-04-16\",https://dolearchivecollections.ku.edu/index.php?p=collections/findingaid&id=51&q=,LSqyFpjhe3g,https://www.youtube.com/watch?v=LSqyFpjhe3g,https://dolearchives.omeka.net/items/show/234,moving image,\"Agriculture and Food, Health, International Affairs\",\"Dole, Robert J., 1923-2021; Baker, Nancy Kassebaum, 1932-\",\"In this 2009 oral history interview with Brien R. Williams, Kassebaum-Baker talks about Senator Dole's work ethic and the issues of abortion, the Panama Canal, and wheat subsidies on the campaign trail.  Baker represented Kansas in the U.S. Senate (1978-1997). She was the first female senator not elected to a seat held by her husband nor appointed to fill out a deceased husband’s term.\",MovingImage\n") }
   end
 
   describe '#to_xml' do
@@ -146,7 +146,7 @@ RSpec.describe ExportResultsPresenter do
             <dcterms:relation>Dole Photograph Collection, 1900-2011</dcterms:relation>
             <dcterms:isPartOf>Collection 012, Box 6, Folder 15</dcterms:isPartOf>
             <dcterms:source>https://dolearchivecollections.ku.edu/index.php?p=collections/controlcard&amp;id=47&amp;q=</dcterms:source>
-            <dcterms:identifier>http://congressarchivesdev.lib.wvu.edu/record/ph_006_015_002</dcterms:identifier>
+            <dcterms:identifier>ph_006_015_002</dcterms:identifier>
             <edm:preview>https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323</edm:preview>
             <edm:isShownAt>https://dolearchives.omeka.net/exhibits/show/voices-from-the-big-first/item/323</edm:isShownAt>
             <dc:type>black-and-white photograph</dc:type>
@@ -158,7 +158,7 @@ RSpec.describe ExportResultsPresenter do
           <item>
             <dcterms:provenance>Robert J. Dole Institute of Politics</dcterms:provenance>
             <dcterms:title>Kassebaum-Baker, Sen. Nancy, 4/16/2009</dcterms:title>
-            <dcterms:date>2009-04-16</dcterms:date>
+            <dcterms:date>2009 April 16</dcterms:date>
             <dcterms:created>2009-04-16</dcterms:created>
             <dcterms:creator>Williams, Brien R.</dcterms:creator>
             <dcterms:rights>http://rightsstatements.org/vocab/CNE/1.0/</dcterms:rights>
@@ -167,7 +167,7 @@ RSpec.describe ExportResultsPresenter do
             <dcterms:relation>Dole Institute Oral History Project, 2002-2009</dcterms:relation>
             <dcterms:isPartOf>Collection 018, Nancy Kassebaum-Baker Oral History from 2009-04-16</dcterms:isPartOf>
             <dcterms:source>https://dolearchivecollections.ku.edu/index.php?p=collections/findingaid&amp;id=51&amp;q=</dcterms:source>
-            <dcterms:identifier>http://congressarchivesdev.lib.wvu.edu/record/LSqyFpjhe3g</dcterms:identifier>
+            <dcterms:identifier>LSqyFpjhe3g</dcterms:identifier>
             <edm:preview>https://www.youtube.com/watch?v=LSqyFpjhe3g</edm:preview>
             <edm:isShownAt>https://dolearchives.omeka.net/items/show/234</edm:isShownAt>
             <dc:type>moving image</dc:type>
