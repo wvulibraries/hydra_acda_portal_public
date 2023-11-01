e148c01e2fc14d4e4235406ae02a4d0ba408e3a0
Kirk Wang <kirk.wang@scientist.com>
2023-11-01 16:43:57 -0700

    🎁 Show only the year in the date range facet
    This commit will make it so that only the year is shown in the date
    range facet which will make it match the input fields.

--- hydra/app/helpers/blacklight_helper.rb
+++ hydra/app/helpers/blacklight_helper.rb
@@ -4,4 +4,19 @@ module BlacklightHelper
   def application_name
     'American Congress Digital Archives Portal'
   end
+
+  def extract_year(date_string)
+    if date_string =~ /\A\d{4}-\d{2}-\d{2}\z/
+      # Matches YYYY-MM-DD
+      Date.strptime(date_string, '%Y-%m-%d').year
+    elsif date_string =~ /\A\d{4}-\d{2}\z/
+      # Matches YYYY-MM
+      Date.strptime(date_string, '%Y-%m').year
+    elsif date_string =~ /\A\d{4}\z/
+      # Matches YYYY
+      Date.new(date_string.to_i).year
+    else
+      date_string
+    end
+  end
 end
