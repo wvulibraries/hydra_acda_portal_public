aac46cbf5b11bb6ea5a71f4317b16623feab6e2b
Kirk Wang <kirk.wang@scientist.com>
2023-11-01 13:55:14 -0700

    🐛 Add collection title to citation
    This commit will add the collection title, which is the dcterms:relation
    to the citation.
    
    Ref:
      - https://github.com/scientist-softserv/west-virginia-university/issues/114

--- hydra/app/services/chicago_citation_service.rb
+++ hydra/app/services/chicago_citation_service.rb
@@ -7,6 +7,7 @@ module ChicagoCitationService
       title = sanitize_value(document['title_tesim']&.first || document['description_tesim']&.first)
       date = sanitize_value(document['date_tesim']&.first)
       location = sanitize_value(document['physical_location_tesim']&.first)
+      collection_title = sanitize_value(document['collection_title_tesim']&.first)
       contributing_institution = sanitize_value(document['contributing_institution_tesim']&.first)
       url = original_url
       access_date = Date.today.strftime("%B %d, %Y")
@@ -14,9 +15,9 @@ module ChicagoCitationService
       text << "<i>#{title}</i>, " if title.present?
       text << "#{date}, " if date.present?
       text << "#{location}, " if location.present?
+      text << "#{collection_title}, " if collection_title.present?
       text << "#{contributing_institution}. " if contributing_institution.present?
-      text << "#{url}"
-      text << " (accessed #{access_date})."
+      text << "#{url} (accessed #{access_date})."
 
       text.strip.html_safe
     end
