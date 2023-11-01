3d1ab8918e617d0fbfee98cda647a4528f6c7ab4
Kirk Wang <kirk.wang@scientist.com>
2023-11-01 16:03:16 -0700

    🐛 Update #add_date to handle more cases
    This commit will update the #add_date method to handle cases such as:
    
    YYYY
    YYYYs
    YYYY's
    YYYY-MM
    YYYY-MM-DD
    No date provided (blank string)
    
    Ref:
      - https://github.com/scientist-softserv/west-virginia-university/issues/112

--- hydra/app/indexers/indexer.rb
+++ hydra/app/indexers/indexer.rb
@@ -8,11 +8,32 @@ class Indexer < ActiveFedora::IndexingService
   private
 
     def add_date(solr_doc)
-      # The allowed date formats are either YYYY, YYYY-MM, or YYYY-MM-DD
-      # the date must be formatted as a 4 digit year in order to be sorted.
-      valid_date_formats = /\A(\d{4})(?:-\d{2}(?:-\d{2})?)?\z/
       date_string = solr_doc['edtf_ssi']
-      year = date_string&.match(valid_date_formats)&.captures&.first
-      solr_doc['date_ssi'] = year if year
+
+      # If there is no date, set it to 'unknown', otherwise it would be blank and show up first in an ascending sort
+      return solr_doc['date_ssi'] = 'unknown' if date_string.blank?
+
+      # Check for 'YYYYs' or "YYYY's" format and convert it to just 'YYYY'
+      year_match = date_string&.match(/\b(\d{4})(?:'s|s)\b/)
+      return solr_doc['date_ssi'] = year_match[1] if year_match # Just the year
+
+      begin
+        # Use Date.parse for other cases
+        date = Date.parse(date_string)
+        # Determine the format needed based on the precision of the original date string
+        if date_string.match(/\b\d{4}-\d{2}-\d{2}\b/)
+          formatted_date = date.strftime('%Y-%m-%d') # YYYY-MM-DD
+        elsif date_string.match(/\b\d{4}-\d{2}\b/)
+          formatted_date = date.strftime('%Y-%m') # YYYY-MM
+        else
+          formatted_date = date.strftime('%Y') # YYYY
+        end
+        solr_doc['date_ssi'] = formatted_date
+      rescue ArgumentError
+        # If Date.parse fails to parse the date_string, it could be a simple year or an unsupported format.
+        # In such a case, fallback to regex to extract the year.
+        year_match = date_string&.match(/\b(\d{4})\b/)
+        solr_doc['date_ssi'] = year_match[1] if year_match
+      end
     end
 end
