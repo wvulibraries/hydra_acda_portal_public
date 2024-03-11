47fbae4b7a09aa6e07e0bca86f1f518a88ed1b73
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2024-03-11 12:41:46 -0400

    Update sidekiq.rb
    updated sidekiq to use redis user and password

--- hydra/config/initializers/sidekiq.rb
+++ hydra/config/initializers/sidekiq.rb
@@ -1,12 +1,14 @@
+sidekiq_config = { url: "redis://#{ENV['REDIS_USER']}:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}" }
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL_SIDEKIQ") { "redis://redis:6379/12" } }
-  schedule_file = "config/schedule.yml"
-  if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
-  config.logger.level = Logger.const_get(ENV.fetch('LOG_LEVEL', 'info').upcase.to_s)
+    config.redis = sidekiq_config
+    schedule_file = "config/schedule.yml"
+    if File.exists?(schedule_file)
+        Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
+    config.logger.level = Logger.const_get(ENV.fetch('LOG_LEVEL', 'info').upcase.to_s)
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL_SIDEKIQ") { "redis://redis:6379/12" } }
+    config.redis = sidekiq_config
 end
