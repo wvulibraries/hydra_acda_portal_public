ce5f9fda9c734dc4a75e4a7a534c9c19f70f4c08
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-12-09 15:10:03 -0500

    current updates

--- hydra/app/controllers/image_viewer_controller.rb
+++ hydra/app/controllers/image_viewer_controller.rb
@@ -4,7 +4,14 @@ class ImageViewerController < ApplicationController
   def index
     id = File.basename(params[:id], File.extname(params[:id]))
     image_model = Acda.where(id: id).first
-    @image = image_model.image_file.content
+
+    if image_model.image_file
+      @image = image_model.image_file.content
+    else
+      # we need to display a default image
+      @image = File.open(Rails.root.join('app', 'assets', 'images', 'no-image.png')).read
+    end
+
     render inline: "<%= raw @image %>", layout: false
   end
 
@@ -12,7 +19,14 @@ class ImageViewerController < ApplicationController
   def thumb
     id = File.basename(params[:id], File.extname(params[:id]))
     image_model = Acda.where(id: id).first
-    @thumb = image_model.thumbnail_file.content
+
+    if image_model.thumbnail_file
+      @thumb = image_model.thumbnail_file.content
+    else
+      # we need to display a default image
+      @thumb = File.open(Rails.root.join('app', 'assets', 'images', 'no-image.png')).read
+    end
+
     render inline: "<%= raw @thumb %>", layout: false
   end
 end
