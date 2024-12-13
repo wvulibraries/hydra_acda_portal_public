6a86b0dd56e9fd9cadf56d1a175a29094f8524a8
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-12-13 18:54:05 -0500

    fixed thumbnails. Created new job to download thumbs and attach them to the record.

--- hydra/import/delete_project.rb
+++ hydra/import/delete_project.rb
@@ -2,16 +2,30 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'active_fedora'
 
+# This script will delete all records with the project identifier 'mcppc'
+# This is a destructive operation and should be used with caution
+# This script will prompt the user for confirmation before deleting the records
+
+# set project
+project = 'acda'
+
 # get the identifier 
-puts "Are you sure you want to delete #{Rails.env} ACDA?"
+puts "Are you sure you want to delete #{Rails.env} #{project}?"
 answer = gets.to_s
 answer.downcase!
 answer.strip!
 
 if (answer == 'y' || answer == 'yes' || answer == true || answer == 1)
   # delete the project
-  ActiveFedora::Base.where(project_tesim: 'acda').each { |r| r.delete(eradicate: true) }
-  puts 'Destroyed the project -- ACDA'
+  # records = ActiveFedora::Base.where(project_tesim: project)
+  records = ActiveFedora::Base.all
+  if records.any?
+    records.each { |r| r.delete(eradicate: true) }
+      puts "Destroyed the project -- #{project}"
+    else
+      puts "No records found for project -- #{project}"
+    end
+    puts "Destroyed the project -- #{project}"
 else 
   puts "Aborted."
-end 
+end
