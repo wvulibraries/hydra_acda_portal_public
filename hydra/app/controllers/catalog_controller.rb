22d9ecad4ac81021d3ba68ebffbfad4c2aa9e9e2
LaRita Robinson <larita@scientist.com>
2025-01-06 18:23:38 -0500

    Addresses several QA followup issues
    https://github.com/notch8/west-virginia-university/issues/193
    https://github.com/notch8/west-virginia-university/issues/194

--- hydra/app/controllers/catalog_controller.rb
+++ hydra/app/controllers/catalog_controller.rb
@@ -82,6 +82,9 @@ class CatalogController < ApplicationController
     # facet creator
     # Facets ---------------------------------------------
     # Organized alphabetically - the ordering of the field names is the order of the display
+    # remove the default facets
+    config.facet_fields.clear
+    # Add desired facets
     config.add_facet_field solr_name('collection_title', :facetable), label: 'Collection', link_to_search: :collection_title_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('congress', :facetable), label: 'Congress', link_to_search: :coverage_congress_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('contributing_institution', :facetable), label: 'Contributing Institution', link_to_search: :contributing_institution_ssi, limit: true, show: true, component: true
@@ -96,7 +99,6 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name('language', :facetable), label: 'Language', limit: true, show: true, component: true
     config.add_facet_field solr_name('location_represented', :facetable), label: 'Location Represented', link_to_search: :coverage_spatial_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('names', :facetable), label: 'Names', link_to_search: :names_ssi, limit: true, show: true, component: true
-    config.add_facet_field solr_name('physical_location', :facetable), label: 'Physical Location', link_to_search: :physical_location_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('policy_area', :facetable), label: 'Policy Area', link_to_search: :policy_area_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('publisher', :facetable), label: 'Publisher', link_to_search: :publisher_ssi, limit: true, show: true, component: true
     config.add_facet_field solr_name('record_type', :facetable), label: 'Record Type', limit: true, show: true, component: true
