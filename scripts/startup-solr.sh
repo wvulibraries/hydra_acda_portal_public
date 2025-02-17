c84606298b25983332feceb3409301d8411a6f9a
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-02-17 13:05:12 -0500

    current updates - updated solr

--- scripts/startup-solr.sh
+++ scripts/startup-solr.sh
@@ -3,9 +3,12 @@
 # set terminal 
 export TERM=vt100
 
-chown -R 8983:8983 /var/solr
+cp -r /opt/solr/modules/extraction/lib/* /opt/solr/server/solr-webapp/webapp/WEB-INF/lib/
+cp -r /opt/solr/modules/analysis-extras/lib/* /opt/solr/server/solr-webapp/webapp/WEB-INF/lib/
+
 mkdir -p /var/solr/data/$1
 cp -r /solr9-setup/conf /var/solr/data/$1/conf
 cp /solr9-setup/core.properties /var/solr/data/$1
 cp /solr9-setup/security.json /var/solr/data
+chown -R 8983:8983 /var/solr
 runuser -u solr -- solr start -f
