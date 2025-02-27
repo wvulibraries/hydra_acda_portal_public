5828bd03261aab9d50621d3241a47238fed131b3
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-02-27 14:34:49 -0500

    max_delay doesn't work with this setup trying another solution.

--- hydra/app/jobs/application_job.rb
+++ hydra/app/jobs/application_job.rb
@@ -2,8 +2,7 @@ class ApplicationJob < ActiveJob::Base
   # Retry on Fedora conflicts with exponential backoff
   retry_on Ldp::Conflict, 
     wait: :exponentially_longer, 
-    attempts: 5,
-    max_delay: 30.seconds
+    attempts: 5
 
   # Set timeouts using ActiveFedora configuration
   around_perform do |_job, block|
@@ -12,5 +11,12 @@ class ApplicationJob < ActiveJob::Base
     ) do
       block.call
     end
+  end
+
+  private
+
+  def retry_delay(attempt)
+    # Calculate exponential backoff with a max of 30 seconds
+    [2 ** attempt, 30].min.seconds
   end
 end
