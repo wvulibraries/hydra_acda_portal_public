7ff8ddc5a76403a0be5021ea4db47c7113338469
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-02-27 09:14:21 -0500

    update on exceptions

--- hydra/app/jobs/application_job.rb
+++ hydra/app/jobs/application_job.rb
@@ -1,2 +1,16 @@
 class ApplicationJob < ActiveJob::Base
+  # Retry on Fedora conflicts with exponential backoff
+  retry_on Ldp::Conflict, 
+    wait: :exponentially_longer, 
+    attempts: 5,
+    max_delay: 30.seconds
+
+  # Set timeouts using ActiveFedora configuration
+  around_perform do |_job, block|
+    ActiveFedora::Base.connection_for_pid('temp').with_timeout(
+      ENV.fetch('FEDORA_TIMEOUT', 60).to_i
+    ) do
+      block.call
+    end
+  end
 end
