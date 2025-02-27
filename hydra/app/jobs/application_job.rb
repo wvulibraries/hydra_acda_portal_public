61a78e8824afad2e08b04dfd47a029b93719c8c1
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-02-27 18:14:46 -0500

    current updates

--- hydra/app/jobs/application_job.rb
+++ hydra/app/jobs/application_job.rb
@@ -6,13 +6,18 @@ class ApplicationJob < ActiveJob::Base
 
   # Set timeouts using ActiveFedora configuration
   around_perform do |_job, block|
-    original_timeout = ActiveFedora.fedora.connection.http.read_timeout
-    ActiveFedora.fedora.connection.http.read_timeout = ENV.fetch('FEDORA_TIMEOUT', 60).to_i
+    connection = ActiveFedora.fedora.connection.http
+    original_options = connection.options.dup
+    
+    # Set timeout options
+    connection.options.timeout = ENV.fetch('FEDORA_TIMEOUT', 60).to_i
+    connection.options.open_timeout = ENV.fetch('FEDORA_TIMEOUT', 60).to_i
     
     block.call
   ensure
-    # Reset timeout after job completes
-    ActiveFedora.fedora.connection.http.read_timeout = original_timeout
+    # Reset timeout options after job completes
+    connection.options.timeout = original_options.timeout
+    connection.options.open_timeout = original_options.open_timeout
   end
 
   private
