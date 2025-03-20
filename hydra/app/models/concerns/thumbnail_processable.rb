f8db51a264427048431a4894c505b2dc37c67da7
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-03-20 15:20:36 -0400

    missed concern

--- hydra/app/models/concerns/thumbnail_processable.rb
+++ hydra/app/models/concerns/thumbnail_processable.rb
@@ -14,6 +14,28 @@ module ThumbnailProcessable
     needs_processing && !saved_change_to_thumbnail_file? && !saved_change_to_image_file?
   end
 
+  def needs_thumbnail_download?
+    preview.present? && 
+    is_active_url?(preview) && 
+    (image_file.blank? || saved_change_to_preview?)
+  end
+
+  def update_thumbnail_and_image
+    return if queued_job == 'true'
+    
+    Rails.logger.info "Starting update_thumbnail_and_image for #{id}"
+    Rails.logger.info "Preview: #{preview}, DC Type: #{dc_type}"
+    
+    if needs_thumbnail_download?
+      Rails.logger.info "Queueing DownloadAndSetThumbsJob for #{id}"
+      self.queued_job = 'true'
+      save(validate: false)
+      DownloadAndSetThumbsJob.perform_later(id)
+    else
+      Rails.logger.info "No thumbnail job needed for #{id}"
+    end
+  end
+
   # Keep only thumbnail-specific methods here
   def saved_change_to_thumbnail_file?
     previous_changes.key?('thumbnail_file')
