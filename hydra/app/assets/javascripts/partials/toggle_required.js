77cd3653408dd627a5a17a1541993050751e1352
Tracy A McCormick <tracy.alan.mccormick@gmail.com>
2026-02-10 15:37:40 -0500

    Add null checks before addEventListener in toggle_required.js to prevent errors if elements are missing

--- hydra/app/assets/javascripts/partials/toggle_required.js
+++ hydra/app/assets/javascripts/partials/toggle_required.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const email_field = document.getElementById('mail_to');
   const checkbox = document.getElementById('checkbox');
-  checkbox.addEventListener('change', function() {
-    email_field.required = this.checked
-  });
+  if (checkbox && email_field) {
+    checkbox.addEventListener('change', function() {
+      email_field.required = this.checked;
+    });
+  }
 });
