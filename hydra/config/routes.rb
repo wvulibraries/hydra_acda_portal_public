bfb4ef540e4aefae32f19bb02144d49165edb123
Praneeth Byna <praneeth.byna@mail.wvu.edu>
2026-04-13 15:03:32 -0400

    pushing the fix for partner site health check

--- hydra/config/routes.rb
+++ hydra/config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   get '/validate' => 'validations#upload'
   post '/validate' => 'validations#show'
 
+  post '/url_health/report_down', to: 'url_health#report_down'
+
   # Add routes for the HTML pages
   # partner archived pages?
   # get '/newsletter' => 'pages#newsletter'
